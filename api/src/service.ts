9461a9e78d5000563abf8801c1d9d77dc51e06f8
Daniel Marques <daniel.marques142@gmail.com>
2021-02-22 15:23:27 -0300

    end of day 1

--- api/src/service.ts
+++ api/src/service.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello Brothar" });
+});
+
+app.post("/", (req, res) => {
+  return res.status(201).json({ message: "Dados enviados com sucesso" });
+});
+
 app.listen(3333, () => console.log("Server is running!"));
